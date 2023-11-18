ce7a23e0fee7316ed0dd48b2777c082672b546e6
David Mansel <mansel.david88@gmail.com>
2023-11-19 00:15:11 +0700

    try comment

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Be sure to give the shell script execute permissions with `chmod +x script.sh` if you haven't already.
 
-export STARKNET_RPC="https://starknet-goerli.infura.io/v3/"
-export STARKNET_KEYSTORE="~/.starkli-wallets/deployer/key.json"
-export STARKNET_ACCOUNT="~/.starkli-wallets/deployer/account.json"
+#export STARKNET_RPC="https://starknet-goerli.infura.io/v3/"
+#export STARKNET_KEYSTORE="~/.starkli-wallets/deployer/key.json"
+#export STARKNET_ACCOUNT="~/.starkli-wallets/deployer/account.json"
 
 # *Note: Uncomment the block 2 lines below before using.
 
